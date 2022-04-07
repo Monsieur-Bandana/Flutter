c3b1206f1f7f0ffece48042560c86d680c56eafe
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2022-04-08 00:07:58 +0200

    edited archived

--- prototype/lib/archive.dart
+++ prototype/lib/archive.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+
 import 'package:prototype/newProject/newProjectButton.dart';
+import 'package:prototype/projects.dart';
 
 class Archive extends StatelessWidget {
   String title = "Archiv";
+  List<String> archived = [];
 
   @override
   Widget build(BuildContext context) {
+    int i = archived.length;
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -13,21 +17,33 @@ class Archive extends StatelessWidget {
         primary: true,
       ),
       body: Column(
-        children: [
-          Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: Text("Keine Projekte im Archiv"),
-                ),
-                const Icon(Icons.arrow_downward),
-              ],
-            ),
-          ),
-          AddProjectButton()
-        ],
+        children: [archiveMessage(i), Projects(archived), AddProjectButton()],
       ),
     );
+  }
+}
+
+class archiveMessage extends StatelessWidget {
+  int count;
+
+  archiveMessage(this.count);
+
+  @override
+  Widget build(BuildContext context) {
+    if (count == 0) {
+      return Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              child: Text("Keine Projekte im Archiv"),
+            ),
+            const Icon(Icons.arrow_downward),
+          ],
+        ),
+      );
+    }
+    // TODO: implement build
+    return SizedBox.shrink();
   }
 }
