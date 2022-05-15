ee2d84c31cecd4423f1d6508fba93e2bcb802ba4
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2022-05-15 18:34:33 +0200

    added map function

--- prototype/lib/projectView/projectMap.dart
+++ prototype/lib/projectView/projectMap.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class ProjectMap extends StatelessWidget {
+  LatLng point = LatLng(50.84623, 6.08596);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -8,9 +11,27 @@ class ProjectMap extends StatelessWidget {
       child: Container(
         child: Container(
           margin: const EdgeInsets.all(10.0),
-          child: Image.asset('assets/map.JPG'),
+          child: FlutterMap(
+            options: MapOptions(center: point, zoom: 16),
+            layers: [
+              TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayerOptions(markers: [
+                Marker(
+                    point: point,
+                    builder: (ctx) => Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                        ))
+              ])
+            ],
+          ),
+          width: 350,
+          height: 200,
         ),
-        width: 350,
       ),
     );
   }
