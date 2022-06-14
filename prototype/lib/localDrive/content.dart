037825c4d4598f6edbe4d07c8e0ff7a0bd1ed502
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2022-06-14 12:07:36 +0200

    changes in content file

--- prototype/lib/localDrive/content.dart
+++ prototype/lib/localDrive/content.dart
@@ -1,15 +1,26 @@
 import 'package:prototype/localDrive/status.dart';
 
+/// erzeugt ein Objekt das als Zwischenspeicher für die zukünftig zu speichernden Informationen
+///  dient und später den JSON-Leseprozess vereinfachen soll
 class Content {
   late String projectName = "Default";
   late Enum status = Status.active;
-  late String Auftraggeber = "Default";
+  late String client = "Default";
 
   String get newProjectName => projectName;
 
   set newProjectName(String text) {
     projectName = text;
   }
+
+  set fromJson(Map<String, dynamic> json) {
+    projectName = json['projectName'];
+    status = json['status'];
+    client = json['client'];
+  }
+
+  Map<String, dynamic> toJson() =>
+      {'projectName': projectName, 'status': status, 'client': client};
 }
 
 
